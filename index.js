8c7040415dca1ab6c4b9cc62fa83ff5974f30f2e
euanlimzx <euanlimzx@gmail.com>
2023-05-18 10:22:16 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const path = require("path");
+const ejsMate = require("ejs-mate");
+const express = require("express");
+const app = express();
+
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.json());
+
+app.set("view engine", "ejs");
+app.engine("ejs", ejsMate);
+
+app.use((req, res, next) => {
+  next();
+});
+
+app.get("/", (req, res) => {
+  res.render("nav/homepage.ejs");
+});
+
+app.listen(8080, () => {
+  console.log("listening!");
+});
---
+++ public/js/boilerplate.js
@@ -0,0 +1,9 @@
+AOS.init();
+
+var typed = new Typed("#element", {
+  strings: ["Youth ", "Generation ", "World. "],
+  startDelay: 300,
+  typeSpeed: 120,
+  backSpeed: 80,
+  loop: true,
+});
